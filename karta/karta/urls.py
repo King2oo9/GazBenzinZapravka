d7c76fbbf2eabd0da3705dd7af0e0fa13ac047e0
King2oo9 <120119639+King2oo9@users.noreply.github.com>
2023-02-09 18:56:44 -0800

    template o'zgartirildi lekin tuzatilmadi

--- karta/karta/urls.py
+++ karta/karta/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'), name='asos'),
+    path('', include('main.urls')),
 ]
 
 
