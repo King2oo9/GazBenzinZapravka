d2e735c4defc87abccf5153be5c092d1bd13342d
King2oo9 <120119639+King2oo9@users.noreply.github.com>
2023-02-09 21:14:09 -0800

    settings DIRS

--- karta/karta/settings.py
+++ karta/karta/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'karta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.templates.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
